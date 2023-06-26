d4b5c8db3965c275cfd670c71927865fea9f5665
sanjeevpraja <sanjeevpraja@gmail.com>
2023-06-26 21:32:26 +0545

    stable create and edit modal popup

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-  $('.notification').on('click', function () {
-    $('.flex-pane').toggleClass('active');
-  });
-  $('.flex-pane .close').on('click', function () {
-    $('.flex-pane').toggleClass('active');
-  });
+    $('.notification').on('click', function () {
+        $('.flex-pane').toggleClass('active');
+    });
+    $('.flex-pane .close').on('click', function () {
+        $('.flex-pane').toggleClass('active');
+    });
+});
+$(document).on('click', '.btn-add', function (e) {
+    e.preventDefault();
+    let parent = $(this).siblings('.box-steps');
+    let step = parent.find('.box-step').first();
+    parent.append(step.first().clone());
+});
+$(document).on('click', '.btn-delete', function (e) {
+    e.preventDefault();
+    $(this).closest('.box-step').remove();
 });
