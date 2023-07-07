617c4b87236a6cc3b2f537b5ea073043950497e0
sanjeevpraja <sanjeevpraja@gmail.com>
2023-07-07 13:25:18 +0545

    stable class views

--- exercise/urls.py
+++ exercise/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('list/', views.ExerciseListView.as_view(), name='exercise_list'),
     path('create/', views.ExerciseCreateView.as_view(), name='exercise_create'),
     path('delete/<int:pk>/', views.ExerciseDeleteView.as_view(), name='exercise_delete'),
-    path('detail/<int:pk>/', views.exercise_detail, name='exercise_detail'),
-    path('edit/<int:pk>/', views.exercise_edit, name='exercise_edit'),
+    path('detail/<int:pk>/', views.ExerciseDetailView.as_view(), name='exercise_detail'),
+    path('edit/<int:pk>/', views.ExerciseEditView.as_view(form_prefix='edit'), name='exercise_edit'),
     path('sorry/', views.sorry, name='sorry'),
 ]
--- exercise/views.py
+++ exercise/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 
 from .models import *
 from .forms import ExerciseForm
 from django.core import serializers
 from django.http import JsonResponse
-from django.views.generic import ListView, CreateView, DetailView, DeleteView
+from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView, View
 import json
 
 
@@ -106,40 +106,75 @@ class ExerciseDeleteView(DeleteView):
 #         return JsonResponse(data)
 
 
-def exercise_edit(request, pk):
-    if request.user.is_authenticated:
-        user_group = request.user.groups.all
-        if request.method == 'GET':
-            exercise_obj = Exercise.objects.get(pk=pk)
-            steps = exercise_obj.steps
-            print(type(steps))
-            form = ExerciseForm(instance=exercise_obj, prefix="edit")
-            content = {'form': form, "model": exercise_obj, "group": user_group, "steps": steps}
-            return render(request, '_exercise-edit.html', content)
-        elif request.method == "POST":
-            old_image = Exercise.objects.get(pk=pk)
-            form = ExerciseForm(request.POST, files=request.FILES or None, instance=old_image, prefix="edit")
-            if form.is_valid():
-                form.save()
-                instance = form.save()
-                ser_instance = serializers.serialize('json', [instance, ])
-                return JsonResponse({"instance": ser_instance}, status=200)
-            else:
-                return JsonResponse(form.errors, status=500)
+class ExerciseEditView(UpdateView):
+    model = Exercise
+    context_object_name = 'model'
+    form_class = ExerciseForm
+    template_name = '_exercise-edit.html'
+    form_prefix = None #this is passed in url
+    success_url = reverse_lazy('exercise:exercise')
 
-    else:
-        return redirect('exercise:sorry')
+    def get_form_kwargs(self):
+        kwargs = super(UpdateView, self).get_form_kwargs()
+        if self.form_prefix:
+            kwargs.update({'prefix': self.form_prefix})
+        return kwargs
+
+    # def get_success_url(self):
+    #     exercise_id = self.kwargs['pk']
+    #     return reverse_lazy('exercise:exercise_edit', kwargs={'pk': exercise_id})
+
+    def post(self, request, *args, **kwargs):
+        obj = get_object_or_404(Exercise, id=self.kwargs['pk'])
+        form_data = ExerciseForm(self.request.POST, self.request.FILES, prefix="edit", instance = obj)
+        #update_item = Exercise.objects.filter(id=self.kwargs['pk'])
+        if form_data.is_valid():
+            instance = form_data.save()
+            ser_instance = serializers.serialize('json', [instance, ])
+            return JsonResponse({"exercise": ser_instance}, status=200)
+        else:
+            return JsonResponse(form_data.errors, status=500)
 
 
-def exercise_detail(request, pk):
-    if request.user.is_authenticated:
-        user_group = request.user.groups.all
-        if request.method == 'GET':
-            exercise_obj = Exercise.objects.get(pk=pk)
-            content = {"model": exercise_obj, "group": user_group}
-            return render(request, '_exercise-detail.html', content)
-    else:
-        return redirect('exercise:sorry')
+# def exercise_edit(request, pk):
+#     if request.user.is_authenticated:
+#         user_group = request.user.groups.all
+#         if request.method == 'GET':
+#             exercise_obj = Exercise.objects.get(pk=pk)
+#             steps = exercise_obj.steps
+#             print(type(steps))
+#             form = ExerciseForm(instance=exercise_obj, prefix="edit")
+#             content = {'form': form, "model": exercise_obj, "group": user_group, "steps": steps}
+#             return render(request, '_exercise-edit.html', content)
+#         elif request.method == "POST":
+#             old_image = Exercise.objects.get(pk=pk)
+#             form = ExerciseForm(request.POST, files=request.FILES or None, instance=old_image, prefix="edit")
+#             if form.is_valid():
+#                 form.save()
+#                 instance = form.save()
+#                 ser_instance = serializers.serialize('json', [instance, ])
+#                 return JsonResponse({"instance": ser_instance}, status=200)
+#             else:
+#                 return JsonResponse(form.errors, status=500)
+#
+#     else:
+#         return redirect('exercise:sorry')
+
+
+class ExerciseDetailView(DetailView):
+    model = Exercise
+    context_object_name = 'model'
+    template_name = '_exercise-detail.html'
+
+# def exercise_detail(request, pk):
+#     if request.user.is_authenticated:
+#         user_group = request.user.groups.all
+#         if request.method == 'GET':
+#             exercise_obj = Exercise.objects.get(pk=pk)
+#             content = {"model": exercise_obj, "group": user_group}
+#             return render(request, '_exercise-detail.html', content)
+#     else:
+#         return redirect('exercise:sorry')
 
 
 def sorry(request):
